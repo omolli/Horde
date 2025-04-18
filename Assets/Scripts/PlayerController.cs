a00eef56f63c0564ba3f23bb425dc4b1d106c194
Otso Ollikainen <otso.ollikainen@solteq.com>
2025-04-18 18:12:21 +0300

    Add animations with the placeholder character

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,21 +2,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D _rb;
-    public float movementSpeed;
+    Rigidbody2D _rb;
+    [SerializeField] SpriteRenderer _characterBody;
+    [SerializeField] Animator _animator;
+    [SerializeField] float _movementSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMovement();
-
-        
     }
 
     private void PlayerMovement()
@@ -26,6 +25,15 @@ public class PlayerController : MonoBehaviour
 
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         movement = Vector2.ClampMagnitude(movement, 1.0f);
-        _rb.linearVelocity = movement * movementSpeed;
+        _rb.linearVelocity = movement * _movementSpeed;
+
+        bool isWalking = movement.magnitude > 0f;
+        _animator.SetBool("isWalking", isWalking);
+
+        if (isWalking) {
+            bool flipSprite = movement.x < 0f;
+            _characterBody.flipX = flipSprite;
+        }
+
     }
 }
